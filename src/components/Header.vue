9691d155eb1f885eee22f751fe4822e90f70cbee
Yasin <yasin@gencpro.com>
2019-10-30 15:39:38 +0300

    landing page init

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="row">
+    <div class="col-12">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">E-Fatura İzmir</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li v-for="(item, key) in navs" :key="key" class="nav-item" :class="(item.active) ? 'active' : ''">
+              <router-link :to="item.path" tag="a" class="nav-link"></router-link>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+    data() {
+      return {
+          navs: [
+              {
+                  title: 'E-Fatura',
+                  path: '/',
+                  active: true
+              },
+              {
+                  title: 'Neden E-Fatura',
+                  path: 'neden-efatura',
+                  active: false
+              },
+              {
+                  title: 'Paketler',
+                  path: '/paketler',
+                  active: false
+              },
+              {
+                  title: 'Başvuru',
+                  path: 'efatura-basvuru',
+                  active: false
+              }
+          ]
+      }
+    }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="content">
+      <h1>E-Fatura İzmir</h1>
+
+      <h2>E-Fatura nedir ?</h2>
+      <p>
+        Kağıt faturanın karşılığı bilgileri içeren, uluslararası formatta standart hale getirilmiş, değiştirilemez bir şekilde mühürlenmiş, kâğıt fatura ile aynı hukuki niteliklere sahip elektronik belgedir.</p>
+      <p>
+        <a href="https://google.com.tr/search?query=efatura" target="_blank" title="E-Fatura arama sonuçları">E-Fatura</a> ile faturalarınızı elektronik ortamda güvenli olarak gönderip, alabilir, 10 yıl boyunca yasal olarak saklayabilir ve online şekilde görüntüleyebilirsiniz.
+      </p>
+      
+      <h3>
+        Bilgi almak için ; <a href="tel:0(232) 449 92 01">0(232) 449 92 01</a>
+      </h3>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
